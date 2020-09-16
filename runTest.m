66333a90607e9145a2b2196593a40a457d3d5651
Samuel Narcisse <narcisse118@gmail.com>
2020-09-16 18:14:26 -0400

    moved Test 1, added ../src dependencie, modified runTest to use folder

--- runTest.m
+++ runTest.m
@@ -1,8 +1,7 @@
 %%Run test;
 import matlab.unittest.TestSuite;
 %% Instance of testing class
-testCase1 = Test1;
 %% Run Test
-result = run(TestSuite.fromFile('Test1.m'));
+result = run(TestSuite.fromFolder('test'));
 %% Display Test Results;
 disp(result);
--- test/Test1.m
+++ test/Test1.m
@@ -9,6 +9,7 @@ classdef Test1 < matlab.unittest.TestCase
         function addSolverToPath(testCase)
             testCase.OriginalPath = path;
             addpath(fullfile(pwd, 'src'));
+            addpath(fullfile(pwd, '..','src'));
         end
     end
     
