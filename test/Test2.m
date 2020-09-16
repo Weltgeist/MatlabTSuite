2153ad5367f90c5a9a9015e986eda72fcfa37363
Samuel Narcisse <narcisse118@gmail.com>
2020-09-16 18:14:26 -0400

    added vector imput lengt test

---
+++ test/Test2.m
@@ -0,0 +1,30 @@
+classdef Test2 < matlab.unittest.TestCase
+
+    
+    properties
+        OriginalPath
+    end
+    
+    methods (TestMethodSetup)
+        function addSolverToPath(testCase)
+            testCase.OriginalPath = path;
+            addpath(fullfile(pwd, 'src'));
+            addpath(fullfile(pwd, '..','src'));
+        end
+    end
+    
+    methods (TestMethodTeardown)
+        function restorePath(testCase)
+            path(testCase.OriginalPath);
+        end
+    end
+    
+    methods (Test)
+        function testLengthSolution(testCase)
+            actSolution = length(sqroot([4,9,144]));
+            expSolution = length([2,3,12]);
+            testCase.verifyEqual(actSolution,expSolution,'AbsTol',sqrt(eps));
+        end
+    end
+end
+
