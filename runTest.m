4fd40c004e959c9263547ab74c79b9a8f93c1b19
Samuel Narcisse <narcisse118@gmail.com>
2020-09-16 18:14:26 -0400

    added additional test direct in test folder

--- runTest.m
+++ runTest.m
@@ -3,7 +3,8 @@ import matlab.unittest.TestSuite;
 %% Merging Tests;
 suite1 = TestSuite.fromFolder('test/typeMath');
 suite2 = TestSuite.fromFolder('test/typeOther');
-totalSuite = [suite1 suite2];
+suite3 = TestSuite.fromFolder('test');
+totalSuite = [suite1 suite2 suite3];
 %% Run Test
 result = run(totalSuite);
 %% Display Test Results;
