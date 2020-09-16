56e9ef0fc215eec97c6f368f09569400390ee955
Samuel Narcisse <narcisse118@gmail.com>
2020-09-16 18:14:25 -0400

    added Imaginary values test case

--- Test1.m
+++ Test1.m
@@ -11,6 +11,13 @@ classdef Test1 < matlab.unittest.TestCase
             expSolution = [2,3,12];
             testCase.verifyEqual(actSolution,expSolution,'AbsTol',sqrt(eps));
         end
+        
+        
+        function testImaginarySolution(testCase)
+            actSolution = sqroot([-4,-9,-144]);
+            expSolution = [2i,3i,12i];
+            testCase.verifyEqual(actSolution,expSolution,'AbsTol',sqrt(eps));
+        end
     end
 end
 
